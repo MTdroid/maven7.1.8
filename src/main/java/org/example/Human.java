dc12b01c8fb696b3295334c8c463cc4ff115e875
”Marat <marattagirov2020@gmail.com>
2023-10-07 21:47:22 +0300

    Initial commit

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@ToString
+@Setter
+@AllArgsConstructor
+
+public class Human {
+     String firstName;
+     String lastName;
+
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Human marat = new Human("Marat","Tagirov");
+        System.out.println(marat);
+    }
+
+}
